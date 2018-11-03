0141f46fe36ce75cdac12d5ddcec88fec15394e7
dshramko <da.shramko@gmail.com>
2018-11-03 15:25:59 +0600

    add initial values to token

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -2,6 +2,9 @@ pragma solidity ^0.4.24;
 
 contract MyToken {
   uint public totalSupply;
+  string public name = "My Token";
+  string public symbol = "MYT";
+  string public standard = "ERC20 My Token v1.0";
 
   mapping(address => uint) public balanceOf;
 
--- test/MyToken.js
+++ test/MyToken.js
@@ -1,7 +1,14 @@
 const MyToken = artifacts.require('./MyToken.sol');
 
 contract('MyToken', (accounts) => {
-  it('sets the total supply after deployment', async () => {
+  it('initializes the contract with correct values', async () => {
+    const instance = await MyToken.deployed();
+    assert(instance.name(), 'My Token', 'has the correct name');
+    assert(instance.symbol(), 'MYT', 'has the correct symbol');
+    assert(instance.standard(), 'ERC20 My Token v1.0', 'has the correct standard');
+  })
+
+  it('allocates the initial supply after deployment', async () => {
     const instance = await MyToken.deployed();
     const totalSupply = await instance.totalSupply();
     const adminBalance = await instance.balanceOf(accounts[0]);
