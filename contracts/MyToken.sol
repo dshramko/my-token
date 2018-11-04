c5212e4e5a497b56752cb1deb59ded5ed9e690c0
dshramko <da.shramko@gmail.com>
2018-11-04 15:25:14 +0600

    add transfer method

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -1,10 +1,16 @@
 pragma solidity ^0.4.24;
 
 contract MyToken {
-  uint public totalSupply;
+  uint   public totalSupply;
   string public name = "My Token";
   string public symbol = "MYT";
   string public standard = "ERC20 My Token v1.0";
+
+  event Transfer(
+    address indexed _from,
+    address indexed _to,
+    uint _value
+  );
 
   mapping(address => uint) public balanceOf;
 
@@ -12,5 +18,21 @@ contract MyToken {
   constructor(uint _initialSupply) public {
     balanceOf[msg.sender] = _initialSupply;
     totalSupply = _initialSupply;
+  }
+
+  /**
+  * @dev Transfer token for a specified addresses
+  * @param _to The address to transfer to.
+  * @param _value The amount to be transferred.
+  */
+  function transfer(address _to,  uint _value) public returns(bool success) {
+    require(balanceOf[msg.sender] >= _value);
+
+    balanceOf[msg.sender] -= _value;
+    balanceOf[_to] += _value;
+
+    emit Transfer(msg.sender, _to, _value);
+
+    return true;
   }
 }
--- test/MyToken.js
+++ test/MyToken.js
@@ -15,4 +15,45 @@ contract('MyToken', (accounts) => {
     assert.equal(totalSupply.toNumber(), 1000000, 'sets the totalSuply to 1000000');
     assert.equal(adminBalance.toNumber(), 1000000, 'allocates initial supply to the admin account');
   })
+
+  it('does not transfer tokens', async () => {
+    const instance = await MyToken.deployed();
+
+    // Test 'require' by transferring larger value than the sender's balance
+    try {
+      await instance.transfer.call(accounts[1], 99999999999999999999);
+    } catch(err) {
+      assert.include(err.message, 'revert');
+    }
+  })
+
+  it('return success boolean if transfer successful', async () => {
+    const instance  = await MyToken.deployed();
+    const success = await instance.transfer.call(accounts[1], 200000, {from: accounts[0]});
+    assert.equal(success, true);
+  })
+
+  it('transfer tokens', async () => {
+    const account_one = accounts[0];
+    const account_two = accounts[1];
+    const amount = 200000;
+
+    const instance = await MyToken.deployed();
+    const balance = await instance.balanceOf(accounts[0])
+    assert.equal(balance.valueOf(), 1000000, "10000 wasn't in the first account");
+
+    // transfer 200_000 from accounts[0] to accounts[1]
+    const receipt = await instance.transfer(accounts[1], amount, {from: accounts[0]});
+    const account_one_balance = await instance.balanceOf(accounts[0])
+    const account_two_balance = await instance.balanceOf(accounts[1])
+    assert.equal(account_one_balance.toNumber(), 800000);
+    assert.equal(account_two_balance.toNumber(), 200000);
+
+    // Transfer event tests
+    assert.equal(receipt.logs.length, 1);
+    assert.equal(receipt.logs[0].event, 'Transfer');
+    assert.equal(receipt.logs[0].args._from, accounts[0]);
+    assert.equal(receipt.logs[0].args._to, accounts[1]);
+    assert.equal(receipt.logs[0].args._value.toNumber(), 200000);
+  })
 })
